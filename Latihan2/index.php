5fd1f697a9fdde16d46ea65b027c3958401982b6
Ivanandika21 <54906064+ivanandika21@users.noreply.github.com>
2021-09-19 23:16:36 +0700

    Delete Latihan2 directory

--- Latihan2/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // echo "<form method='get' action='kalkulator.php'>";
-    echo "<form method='post' action='kalkulator.php'>";
-    echo "<input type='number' name='bil1'><br>";
-    echo "<input type='number' name='bil2'><br>";
-    echo "<input type='submit' value='Tambah'><br>";
-    echo "</form>";
-?>
