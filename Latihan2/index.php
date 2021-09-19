22e6abf4870803a947527b7ad2d46a7553799b05
ivanandika21 <ivanandikasurya21@gmail.com>
2021-09-19 23:18:17 +0700

    Latihan 2 - PBP

---
+++ Latihan2/index.php
@@ -0,0 +1,8 @@
+<?php
+    // echo "<form method='get' action='kalkulator.php'>";
+    echo "<form method='post' action='kalkulator.php'>";
+    echo "<input type='number' name='bil1'><br>";
+    echo "<input type='number' name='bil2'><br>";
+    echo "<input type='submit' value='Tambah'><br>";
+    echo "</form>";
+?>
