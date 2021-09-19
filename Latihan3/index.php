8bf31e19631e24013efd99fca49b3bd4b86d11e8
ivanandika21 <ivanandikasurya21@gmail.com>
2021-09-19 23:18:44 +0700

    Latihan 3 - PBP

---
+++ Latihan3/index.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<form method='post' action='ganjilgenap.php'>";
+    echo "<input type='number' name='bil'>";
+    echo "<input type='submit' value='Gass'>";
+    echo "</form>";
+?>
