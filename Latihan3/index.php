a232144de037100eb96606d81014856f016667d2
Ivanandika21 <54906064+ivanandika21@users.noreply.github.com>
2021-09-20 12:19:08 +0700

    Delete Latihan3 directory

--- Latihan3/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "<form method='post' action='ganjilgenap.php'>";
-    echo "<input type='number' name='bil'>";
-    echo "<input type='submit' value='Gass'>";
-    echo "</form>";
-?>
