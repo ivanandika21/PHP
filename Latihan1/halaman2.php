0aa8b9ac11d9d4f28fbcec1591d58699b9225958
Ivanandika21 <54906064+ivanandika21@users.noreply.github.com>
2021-09-20 12:17:22 +0700

    Delete Latihan1 directory

--- Latihan1/halaman2.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // echo "Selamat datang ".$_GET["nama"]."\n";
-    // echo "Password anda adalah ".$_GET["psswd"];
-    echo "Selamat datang ".$_POST["nama"]."\n";
-    echo "Password anda adalah ".$_POST["psswd"];
-?>
