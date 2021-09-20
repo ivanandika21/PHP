ab9c3fa23a708e4ac2b9842d4d7a1eb9f2e2ce9e
ivanandika21 <ivanandikasurya21@gmail.com>
2021-09-20 12:21:41 +0700

    PBP-Latihan4

---
+++ PBP-Latihan4/index.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<form method='post' action='perulangan.php'>";
+    echo "<input type='number' name='bil'>";
+    echo "<input type='submit' value='Gass'>";
+    echo "</form>";
+?>
