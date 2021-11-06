caba72842d8e5492f6a6a64f7fe34994c2df25a2
ivanandika21 <ivanandikasurya21@gmail.com>
2021-11-06 13:19:37 +0700

    Update index

--- PBP-Tugas8/view/page_index.php
+++ PBP-Tugas8/view/page_index.php
@@ -6,26 +6,47 @@
     $no = 1;
 ?>
 
-<table border="1">
-    <a href="../view/form_input.php">Input</a>
-    <tr>
-        <td>No.</td>
-        <td>ID DB</td>
-        <td>NIS</td>
-        <td>Nama Siswa</td>
-        <td>Sunting</td>
-    </tr>
-    <?php foreach ($datasiswa as $key){ ?>
-    <tr>
-        <td><?= $no; ?></td>
-        <td><?= $key["no"]; ?></td>
-        <td><?= $key["nis"]; ?></td>
-        <td><?= $key["nama"]; ?></td>
-        <td>
-            <a href="../view/form_update.php?id_upd=<?= $key["no"]; ?>">Update</a>
-            <a href="../control/prosesdelete.php?id_del=<?= $key["no"]; ?>">Delete</a>
-        </td>
-        <?php $no++; ?>
-    </tr>
-    <?php } ?>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="con">
+            <h2>Pendataan Database</h2>
+            <table class="styled">
+
+                <a class="btn blue" href="../view/form_input.php">Input</a>
+                <tr class="h-color">
+                    <td>No.</td>
+                    <td>ID DB</td>
+                    <td>NIS</td>
+                    <td>Nama Siswa</td>
+                    <td>Sunting</td>
+                </tr>
+                <?php foreach ($datasiswa as $key){ ?>
+                <tr>
+                    <td><?= $no; ?></td>
+                    <td><?= $key["no"]; ?></td>
+                    <td><?= $key["nis"]; ?></td>
+                    <td><?= $key["nama"]; ?></td>
+                    <td>
+                        <a class="btn blue" href="../view/form_update.php?id_upd=<?= $key["no"]; ?>">Update</a>
+                        <a class="btn red" href="../control/prosesdelete.php?id_del=<?= $key["no"]; ?>">Delete</a>
+                    </td>
+                    <?php $no++; ?>
+                </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+</body>
+
+</html>
