94916ec4a971c2e3efaefdb11265190cf09bd1ed
ivanandika21 <ivanandikasurya21@gmail.com>
2021-11-07 21:19:19 +0700

    Kurang css form

--- PBP-Tugas8/view/form_input.php
+++ PBP-Tugas8/view/form_input.php
@@ -1,5 +1,26 @@
-<form method="post" action="../control/prosesinput.php">
-    NIS : <input type="text" name="fns"><br>
-    Nama : <input type="text" name="fnm"><br>
-    <input type="submit" value="Input">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="con">
+            <a href="../view/page_index.php" class="blue aa"></a>
+            <h2>Form Input</h2>
+            <form method="post" action="../control/prosesinput.php">
+                <input type="text" name="fns" placeholder="Masukkan NIS"><br>
+                <input type="text" name="fnm" placeholder="Masukkan Nama"><br>
+                <input type="submit" value="Input" class="btn blue">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
